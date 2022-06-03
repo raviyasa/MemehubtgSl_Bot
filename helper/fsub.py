917aec7d335eebd09f5547a83a2f9d9343c57c9e
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-06-03 10:02:32 +0530

    Update fsub.py

--- helper/fsub.py
+++ helper/fsub.py
@@ -8,7 +8,7 @@ async def forcesub(bot, update):
             await bot.get_chat_member(force_subchannel, update.from_user.id)
         except UserNotParticipant:
             file_id = "CAADBQADOAcAAn_zKVSDCLfrLpxnhAI"
-            return await bot.send_sticker(message.chat.id, file_id)
+            return await bot.send_sticker(update.chat.id, file_id)
             text = FORCESUB_TEXT
             reply_markup = FORCESUB_BUTTONS
             await bot.send_message(update.from_user.id,
