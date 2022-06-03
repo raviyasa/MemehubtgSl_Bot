792393a07519f7fc986543b85c36f343302c2bbf
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-06-03 16:12:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ import heroku3
 import requests
 from helper.heroku_helper import HerokuHelper
 from helper.fsub import forcesub
+from PyTgAlpha.source import *
 #--------------------------------------------------Db-------------------------------------------------#
 
 
