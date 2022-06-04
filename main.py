327b71dd26d59202a05d9cbb24bf1d68043fe410
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-06-04 21:28:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -791,10 +791,6 @@ async def replay_media(bot, message):
         )
 print("Pm Working....")
 
-@Client.on_message(filters.command("genlink"))
-async def reply_text(bot, message):
-    link = await bot.create_chat_invite_link(message.chat.id, name=message.from_user.username)
-    await message.reply_text(text=f"**Link Generated, request by {message.from_user.mention}\n\n**Link:** {link.invite_link}\n**Generated Date And Time:** `{link.date}`")
 
 #------------------------------------------------Callback-------------------------------------------#
 @Client.on_callback_query()  
